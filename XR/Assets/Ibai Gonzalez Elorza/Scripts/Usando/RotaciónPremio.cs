75f241c86ca293a209791a3ce62a8a47ba58b8a3
Gonzal <ibai_gonzalez@euneic.com>
2026-03-04 20:14:29 +0100

    hjb

--- XR/Assets/Ibai Gonzalez Elorza/Scripts/Usando/RotaciónPremio.cs
+++ XR/Assets/Ibai Gonzalez Elorza/Scripts/Usando/RotaciónPremio.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RotaciónPremio : MonoBehaviour
 {
+    Vector3 initialPos;
+    Vector3 endPos = new Vector3(10, 0, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,8 @@ public class RotaciónPremio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        initialPos = this.transform.rotation;
+        this.transform.position = Vector3.Lerp(initialPos, endPos, 3f);
+        endPos = new Vector3(endPos.x++, endPos.y++, endPos.z++);
     }
 }
